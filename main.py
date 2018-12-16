3d78c9caca167ec48f08757505da0689a22e16e6
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-12-16 21:45:01 +0500

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+# from blaze import data
+# from blaze.utils import example
+import os
+
+# with open("data.csv") as fd:
+#     for i in fd:
+#         print(i)
+# if __name__ == '__main__':
+#     db_file_path = os.path.abspath('db.hdf5')
+#     iris = data(db_file_path)
+#     print(
+#     iris.peek()
+#     )
+
+print(str(dir(os)))
