ba1ded3aeb8a09a5493c1acc982323382f63abfb
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-12-23 17:11:26 +0500

    main

---
+++ h5_tutorial.py
@@ -0,0 +1,17 @@
+import h5pyd
+import numpy as np
+
+# get handle to domain object
+f = h5pyd.File("db.public.h5.svr", "r+", endpoint="http://h5.svr:5000")
+# this is the root group uuid
+dset = f['default']
+dset[1,1] = 99999
+print(
+    f.id.uuid,
+    f.keys(),
+    f['default'][:]
+)
+
+# g = f.create_group('Base_Group3')
+# print(g)
+# g.attrs['workers'] = 4
