b2aa400478a5e999d264d6137e4eb6285908a87b
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-12-23 19:00:41 +0500

    This is old and not working code

--- h5client.py
+++ h5client.py
@@ -6,11 +6,20 @@ import json
 def get_all(name):
    print(name)
 
-domain = "/home/test_user1/myfile"
-endpoint = "http://192.168.88.192:5000"
+# domain = "/datasets/f5d7f2fc-0147-11e9-96a8-080027809a89"
+domain = ''
+endpoint = "http://h5.svr:5000"
 
-f = h5pyd.File(domain, 'r', endpoint=endpoint, username='test_user1', password='test')
+f = h5pyd.File(domain, mode='r', endpoint=endpoint, username='test_user1', password='test')
 print(f.id.uuid)
+arr = np.array([1,2,3,4])
+# f['value']
+# g = f.create_group('Base_Group3')
+# g.attrs['workers'] = 4
+NUM = 1200
+# create the dataset
+# dset = g.create_dataset('pts', (), dtype='f8')
+# f['home']
 # f.visit(get_all)
 # g = f['myfile.h5']
 # for k in g.attrs.keys():
@@ -18,3 +27,13 @@ print(f.id.uuid)
 
 # ds_local = f["0"]
 # dset = f.create_dataset('dset', dims, dtype='int8')
+
+
+dims = [16, 16, 16]
+filename = 'db.hdf5'
+f = h5pyd.File(filename, "w")
+
+print("create dataset")
+
+
+dset = f.create_dataset('dset', dims, dtype='int8')
