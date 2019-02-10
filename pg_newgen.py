16f2ca2a6d8984702713319e08e0414e051612a5
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2019-02-10 21:28:14 +0500

    same update

--- pg_newgen.py
+++ pg_newgen.py
@@ -1,6 +1,5 @@
-import psycopg2
-import psycopg2.extras
 import numpy as np
+import psycopg2.extras
 
 dbname = 'geosim'
 user = 'postgres'
@@ -8,15 +7,14 @@ host = '192.168.88.192'
 password = '1'
 
 
-
-
-
 def upload(conn):
-    #upload file
+    # upload file
     with conn:
         with conn.cursor() as curs:
-            loid =conn.lobject(0,'r', 0, 'db.hdf5')
+            loid = conn.lobject(0, 'r', 0, 'db.hdf5')
             print('oid', loid.oid)
+
+
 def download(conn):
     # download file
     with conn:
@@ -26,7 +24,7 @@ def download(conn):
 
 
 def upload_from_numpy(conn):
-    dim = (10, 10, 10)
+    dim = (2024, 2024, 1024)
     arr = np.random.rand(*dim)
 
     with conn:
@@ -35,6 +33,23 @@ def upload_from_numpy(conn):
             np.save(lobj, arr, allow_pickle=False)
             print(lobj.oid)
 
+
+def upload_from_numpy_big(conn):
+    import io
+    from io import BytesIO, TextIO
+    dim = (100, 100, 100)
+    arr = np.random.rand(*dim)
+    chunk_size = 64 * 1024 * 1024
+    with conn:
+        with conn.cursor() as curs:
+            lobj = conn.lobject()
+            tmp_byte = io.BytesIO(b'')
+            # with open(tmp_byte, 'rb+') as fd:
+            np.save(tmp_byte, arr, allow_pickle=False)
+            while lobj.write(tmp_byte.read(chunk_size)): pass
+            print(lobj.oid)
+
+
 def download_to_numpy(conn):
     with conn:
         with conn.cursor() as curs:
@@ -42,8 +57,6 @@ def download_to_numpy(conn):
             data = np.load(lobj, allow_pickle=False)
             print(lobj.oid, data.shape)
             return data
-
-
 
 
 if __name__ == '__main__':
@@ -55,11 +68,10 @@ if __name__ == '__main__':
 
     SQL = 'select data from pg_largeobject where loid=16389'
 
-    array = download_to_numpy(conn)
-    print(array)
+    # array = download_to_numpy(conn)
+    # print(array)
 
-
-
-
+    # upload(conn)
+    upload_from_numpy_big(conn)
 
     conn.close()
